81fbf8a2214bce2b089eea1a2a41946efcbc16c7
Seocum <siok1joel@gmail.com>
2024-11-20 17:44:56 -0500

    Practica Juego

---
+++ src/Game/Game.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Game;
+
+import javax.swing.*;
+
+public class Game {
+    public static void main(String[] args) {
+        // Crear la ventana del juego
+    }
+}
